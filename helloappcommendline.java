0886b1e8238bec89f45d3656e85a9114af075650
rish-u07 <rishu2007s@gmail.com>
2026-03-18 15:28:43 +0530

    first coomit

---
+++ helloappcommendline.java
@@ -0,0 +1,12 @@
+package JAVA;
+
+public class helloappcommendline{
+      public static void main(String[] args) {
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+    }
+}
